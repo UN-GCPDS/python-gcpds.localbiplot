abefc956af8b6c121f4c7f62d5f88202b110225f
Jenniffer C Triana Martinez <jectrianama@unal.edu.co>
2024-03-19 15:23:05 -0500

    Create __init__.py

---
+++ gcpds/localbiplot/__init__.py
@@ -0,0 +1,10 @@
+# example.py
+
+def greet(name):
+    """
+    This function greets the person whose name is passed as a parameter
+    """
+    return f"Hello, {name}!"
+
+if __name__ == "__main__":
+    print(greet("World"))
